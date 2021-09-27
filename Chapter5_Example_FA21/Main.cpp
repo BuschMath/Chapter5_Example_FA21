f1a8ae665d186d838e4eed99012a8cc3b3035946
BuschMath <busch@iowacentral.edu>
2021-09-27 10:35:24 -0500

    File IO Example

--- Chapter5_Example_FA21/Main.cpp
+++ Chapter5_Example_FA21/Main.cpp
@@ -1,55 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Do you want to determine your outcome by letter or number? Enter l for letter or n for number.\n";
-	char test;
-	bool letter_grade;
+	int h, w;
+	ifstream infile;
+	infile.open("input.dat");
 
-	cin >> test;
-
-	if (test == 'l')
+	if (!infile)
 	{
-		letter_grade = true;
-	}
-	else
-	{
-		letter_grade = false;
+		cout << "Cannot open file!\n";
+		return 1;	// terminate the program
 	}
 
-	if (letter_grade)
-	{
-		char Grade;
-		cout << "Enter your grade: ";
-		cin >> Grade;
-		cout << endl;
-
-		if (Grade == 'A' || Grade == 'B' || Grade == 'C' || Grade == 'a' || Grade == 'b' || Grade == 'c')
-		{
-			cout << "Congrats! You passed. \n\n";
-		}
-		else
-		{
-			cout << "That's sad. You failed. \n\n";
-		}
-	}
-	else
-	{
-		double Grade_Number;
-		cout << "Enter your grade: ";
-		cin >> Grade_Number;
-
-		if (Grade_Number >= 70)
-		{
-			cout << "Congrats! You passed. \n\n";
-		}
-		else
-		{
-			cout << "That's sad. You failed. \n\n";
-		}
-	}
+	infile >> h >> w;
 
 	return 0;
 }
